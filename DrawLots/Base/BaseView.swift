d1b5b4f806252a18579cdb5c31695469922b1d4f
bey.ss <sergeybey17@gmail.com>
2022-05-29 23:10:26 +0300

    [1#] added dependencies and base views

---
+++ DrawLots/Base/BaseView.swift
@@ -0,0 +1,37 @@
+//
+//  BaseView.swift
+//  DrawLots
+//
+//  Created by Сергей  Бей on 29.05.2022.
+//
+
+import UIKit
+
+public struct Appearance {
+    let baseOffset: CGFloat = 16
+}
+
+open class BaseView: UIView {
+    
+    public override init(frame: CGRect) {
+        super.init(frame: frame)
+        setupUI()
+    }
+    
+    public required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    open var appearance = Appearance()
+    
+    //MARK: - Setup User Interface
+    open func setupUI() {
+        addSubviews()
+        makeConstraints()
+        configureUI()
+    }
+    
+    open func addSubviews() {}
+    open func makeConstraints() {}
+    open func configureUI() {}
+}
--- DrawLots/Base/BaseViewController.swift
+++ DrawLots/Base/BaseViewController.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  BaseViewController.swift
 //  DrawLots
 //
 //  Created by Сергей  Бей on 29.05.2022.
@@ -7,13 +7,12 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class BaseViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        view.backgroundColor = .orange
     }
-
 
 }
 
